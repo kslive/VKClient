211d320fb07f349221fb8dca36c853b84e55d5f2
ksllive <zhenya-kiselev-97@mail.ru>
2020-07-29 14:12:48 +0300

    Design.

---
+++ VK Client/LoginViewController.swift
@@ -0,0 +1,24 @@
+//
+//  LoginViewController.swift
+//  VK Client
+//
+//  Created by Eugene Kiselev on 29.07.2020.
+//  Copyright © 2020 Eugene Kiselev. All rights reserved.
+//
+
+import UIKit
+
+class LoginViewController: UIViewController {
+
+    @IBOutlet weak var loginTextField: UITextField!
+    @IBOutlet weak var passwordTextField: UITextField!
+    @IBOutlet weak var signInButton: UIButton!
+    
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+    
+    @IBAction func pressedSignInButton(_ sender: UIButton) {
+    }
+}
