89b31fd3eeb9e9f0ae07be9784b8d9dff91bf63a
ksllive <zhenya-kiselev-97@mail.ru>
2020-10-01 16:32:55 +0300

    add search group from model

--- VK Client/Groups/AvailableGroups/Controller/AvailableGroupsController+UISearchController.swift
+++ VK Client/Groups/AvailableGroups/Controller/AvailableGroupsController+UISearchController.swift
@@ -1,26 +1,23 @@
 //
-//  MyGroupsController+UISearchController.swift
+//  AvailableGroupsController+UISearchController.swift
 //  VK Client
 //
-//  Created by Eugene Kiselev on 31.08.2020.
+//  Created by Eugene Kiselev on 01.10.2020.
 //  Copyright © 2020 Eugene Kiselev. All rights reserved.
 //
 
 import UIKit
 
-extension MyGroupsController: UISearchResultsUpdating {
+// sorry for that @Leonid
+extension AvailableGroupsController: UISearchResultsUpdating, UISearchBarDelegate {
     
     func setupSearchController() {
     
-// Получаем инфо об изменении текста:
         searchController.searchResultsUpdater = self
-// Запрет взаимодействия с контентом при вводе:
+        searchController.searchBar.delegate = self
         searchController.obscuresBackgroundDuringPresentation = false
-// Название строки поиска:
         searchController.searchBar.placeholder = "Search Groups"
-// Расположение:
         navigationItem.searchController = searchController
-// Строка поиска при переходе:
         definesPresentationContext = true
     }
     
@@ -32,12 +29,21 @@ extension MyGroupsController: UISearchResultsUpdating {
     
     func filterContentForSearchText(_ searchText: String) {
         
-        filteredGroups = myGroups.filter{ (group: Group) -> Bool in
-            
+        fetchRequestSearchGroups(text: searchText)
+        allGroups = allGroups.filter{ (group: Group) -> Bool in
+
             guard let name = group.name else { return false }
             return name.contains(searchText)
         }
-        
+    }
+    
+    func searchBarTextDidBeginEditing(_ searchBar: UISearchBar) {
+        allGroups.removeAll()
+        tableView.reloadData()
+    }
+    
+    func searchBarCancelButtonClicked(_ searchBar: UISearchBar) {
+        allGroups.removeAll()
         tableView.reloadData()
     }
 }
--- VK Client/Groups/AvailableGroups/Controller/AvailableGroupsController.swift
+++ VK Client/Groups/AvailableGroups/Controller/AvailableGroupsController.swift
@@ -10,11 +10,43 @@ import UIKit
 
 class AvailableGroupsController: UITableViewController {
     
-// Дефолтный массив:
+    let searchController = UISearchController(searchResultsController: nil)
+    let networkManager = NetworkManager()
     var allGroups = [Group]()
+    var searchBarIsEmpty: Bool {
+        
+        guard let text = searchController.searchBar.text else { return false }
+        
+        return text.isEmpty
+    }
+    var isFiltering: Bool {
+        
+        return searchController.isActive && !searchBarIsEmpty
+    }
 
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        setupSearchController()
+    }
+    
+    // MARK: Help Function
+    
+    func fetchRequestSearchGroups(text: String?) {
+        
+        networkManager.fetchRequestSearchGroups(text: text) { [weak self] groups in
+                
+                DispatchQueue.main.async {
+                    
+                    if self!.isFiltering {
+                        
+                        self?.allGroups = groups
+                    }
+                    
+                    self?.tableView.reloadData()
+                }
+            
+        }
     }
 
     // MARK: - Table view data source
@@ -24,15 +56,24 @@ class AvailableGroupsController: UITableViewController {
     }
 
     override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        return allGroups.count
+        
+        if isFiltering {
+            return allGroups.count
+        }
+        
+        return 0
     }
 
     override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         
         let cell = tableView.dequeueReusableCell(withIdentifier: "AvailableGroupsCell", for: indexPath) as! AvailableGroupsCell
+        
         let allGroup = allGroups[indexPath.row]
         
-        cell.configure(for: allGroup)
+        if isFiltering {
+            
+            cell.configure(for: allGroup)
+        }
         
         return cell
     }
